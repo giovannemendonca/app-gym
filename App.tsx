2f43cc0b71ff28abbf5553e0acac9d4004620278
Giovanne Mendonca <giovannemendonca@outlook.com>
2023-08-18 22:32:08 -0300

    install native-base

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import { Text, View, StatusBar} from "react-native";
+import { NativeBaseProvider } from "native-base";
+import { View, StatusBar } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
@@ -6,18 +7,18 @@ import {
 } from "@expo-google-fonts/roboto";
 
 export default function App() {
-  const [fontsLoaded] =  useFonts({
+  const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_700Bold
-  })
+    Roboto_700Bold,
+  });
   return (
-    <View style={{alignItems:'center', justifyContent: 'center', flex:1, backgroundColor: '#202024'}}>
-      <StatusBar 
+    <NativeBaseProvider>
+      <StatusBar
         barStyle="light-content"
         backgroundColor={"transparent"}
         translucent
-/>
-      {fontsLoaded ? <Text>App-Gyms</Text> : <View />}
-    </View>
+      />
+      {fontsLoaded ? <View /> : <View />}
+    </NativeBaseProvider>
   );
 }
